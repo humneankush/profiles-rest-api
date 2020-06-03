a60760242bdfe708fd0a3a577ff12e4db3a438ca
ankush humane <anikshahumne@gmail.com>
2020-06-03 16:47:35 +0530

    register the model with admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -19,7 +19,7 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_super_user(self, email, name, password):
+    def create_superuser(self, email, name, password):
         """create and save a new superuser with given detail"""
         user = self.create_user(email, name, password)
 
